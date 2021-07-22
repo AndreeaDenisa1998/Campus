d63e71dd56a6a5610703564aba7847fd20970451
SoltuzAlexandraIsabela <63060706+SoltuzAlexandraIsabela@users.noreply.github.com>
2021-07-22 15:46:20 +0300

    register button as formgroup

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -15,11 +15,13 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm=new FormGroup({
+      name:new FormControl('',Validators.required),
+      email:new FormControl('',Validators.required),
       username:new FormControl('',Validators.required),
       password:new FormControl('',Validators.required),
     });
   }
-  onSubmit(): void{
+  onSubmitLogin(): void{
     //this.router.navigateByUrl('account');
     const username=this.loginForm.get('username')?.value;
     const password=this.loginForm.get('password')?.value;
@@ -29,6 +31,18 @@ export class LoginComponent implements OnInit {
         //this.router.navigateByUrl()
         this.router.navigateByUrl('account');
     }  
-
   }
+  onSubmitRegister(): void{
+    //this.router.navigateByUrl('account');
+    const name=this.loginForm.get('name')?.value;
+    const email=this.loginForm.get('email')?.value;
+    const username=this.loginForm.get('username')?.value;
+    const password=this.loginForm.get('password')?.value;
+  
+    if(name !=='' && email !== '' && username !=='' && password !=='')
+    {
+        //this.router.navigateByUrl()
+        this.router.navigateByUrl('account');
+    }  
+    }
 }
--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    this.registerForm=new FormGroup({
+      name:new FormControl('',Validators.required),
+      email:new FormControl('',Validators.required),
+      username:new FormControl('',Validators.required),
+      password:new FormControl('',Validators.required),
+    });
   }
-
+  onSubmitRegister(): void{
+    //this.router.navigateByUrl('account');
+    const name=this.registerForm.get('name')?.value;
+    const email=this.registerForm.get('email')?.value;
+    const username=this.registerForm.get('username')?.value;
+    const password=this.registerForm.get('password')?.value;
+  
+    if(name !=='' && email !== '' && username !=='' && password !=='')
+    {
+        //this.router.navigateByUrl()
+        this.router.navigateByUrl('account');
+    }  
+    }
 }
+
+
